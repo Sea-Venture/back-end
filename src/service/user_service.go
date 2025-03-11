d892e6419a6481865b4b37e596c4ddafbd5482f1
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2025-03-12 01:03:36 +0530

    backup the code

--- src/service/user_service.go
+++ src/service/user_service.go
@@ -30,7 +30,6 @@ func RegisterUser(user *models.User) error {
 }
 
 func Login(user *models.User) error {
-
 	existingUser, err := repository.GetUserByEmail(user.Email)
 	if err != nil {
 		return errors.New("invalid credentials")
@@ -42,6 +41,7 @@ func Login(user *models.User) error {
 	}
 
 	user.ID = existingUser.ID
+	user.Role = existingUser.Role
 
 	return nil
 }
@@ -54,5 +54,5 @@ func UpdateProfilePic(user *models.User, profilePic string) error {
 }
 
 func GetUserById(id uint) (*models.User, error) {
-    return repository.GetUserById(id)
+	return repository.GetUserById(id)
 }
